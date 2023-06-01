76df8e4fbd00549c8a8d8ea7ed2d5e507005048f
saradonnelly97 <sara.donnelly8@gmail.com>
2023-05-31 23:07:26 -0400

    listBooks fetch

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,8 @@
 
 // Your Code Here
+
+async function main(){
+    let response = await fetch("http://localhost:3001/listBooks")
+    let books = await response.json()
+    books.forEach(renderBook)
+}
